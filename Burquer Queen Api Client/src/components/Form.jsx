016dd5ac6ed479110760eddb6269677694694523
Isabel Lira <isabell.lira@gmail.com>
2023-07-21 13:41:58 -0400

    crea login HU1

---
+++ Burquer Queen Api Client/src/components/Form.jsx
@@ -0,0 +1,39 @@
+import "./Form.css"
+import { useState } from "react"
+export function Form() {
+    const [correo, setCorreo] = useState("")
+    const [contraseña, setContraseña] = useState("")
+    const [error, setError] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if(correo === "" || contraseña === "") {
+            setError(true)
+            return
+        }
+    }
+    
+    return (
+        <section>
+            <h1>Login</h1>
+            <form 
+                className= "formLogin"
+                onSubmit={handleSubmit}
+            >
+                <input 
+                type="text"
+                value={correo}
+                onChange= {e => setCorreo(e.target.value)}
+                />
+                <input type="password"
+                value={contraseña}
+                onChange= {e => setContraseña(e.target.value)}
+                />
+
+                <button>Iniciar sesión</button>
+            </form>
+            {error && <p>ERROR: Revisa tu correo o contraseña</p>}
+        </section>
+    )
+}
