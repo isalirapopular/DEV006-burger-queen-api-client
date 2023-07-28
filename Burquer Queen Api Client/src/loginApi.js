78976ad9f649b0777cd490b730ee856bbdce3b77
Isabel Lira <isabell.lira@gmail.com>
2023-07-28 10:08:45 -0400

    vista login con consumo de api sin testear

---
+++ Burquer Queen Api Client/src/loginApi.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+ export async function login (email, password) { 
+    const { data } = await axios.post('http://localhost:8080/login', {
+              email,
+              password,
+            });
+            console.log(data, "AQUÍ ESTÁ LA DATA")
+            return data;
+        };
+        
