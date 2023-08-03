f4b031f1ae1991811e2a6b4fe5a1a53cb1df197e
Isabel Lira <isabell.lira@gmail.com>
2023-08-03 13:35:09 -0400

    crea componenente selected Products

--- Burquer Queen Api Client/src/components/Home.jsx
+++ Burquer Queen Api Client/src/components/Home.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
 import { products } from "./GetApi.js"
 import { ProductFilter } from "./ProductFilter.jsx";
+import SelectedProducts from "./SelectedProducts.jsx"
 import './Home.css'
 
 export function Home({ user, setUser }) {
   const [productsData, setProductsData] = useState([])
   const [filteredProducts, setFilteredProducts] = useState([]); // Agrega el estado para los productos filtrados
-  const [SelectedProducts, setSelectedProducts] = useState([]);
+  const [selectedProducts, setSelectedProducts] = useState([]);
+  
   function getProducts() {
     console.log(products)
     products(user.token)
@@ -24,33 +26,34 @@ export function Home({ user, setUser }) {
   }, [])
 
   const handleButtonClick = (product) => {
-    setSelectedProducts(prevSelectedProducts => [...prevSelectedProducts, product]);
+    setSelectedProducts((prevSelectedProducts) => [...prevSelectedProducts, product]);
   };
   return (
     <div className="main">
       <header className="navHome">
-        <button className='buttonCerrarSeccion' onClick={handleLogout}>Cerrar Sesion</button>
-        <ProductFilter productsData={productsData} setFilteredProducts={setFilteredProducts} />
+        <button className="buttonCerrarSeccion" onClick={handleLogout}>
+          Cerrar Sesion
+        </button>
+        <ProductFilter
+          productsData={productsData}
+          setFilteredProducts={setFilteredProducts}
+        />
       </header>
       <section className="sectionBody">
-      <section className="sectionProductos">
-        {filteredProducts.map((product) => (
-          <button
-            className='buttonProductos'
-            key={product.id}
-            onClick={() => handleButtonClick(product)}>
-            {product.name} ${product.price}
-          </button>
-        ))}
-      </section>
-      <div className="selectedProducts">
-        <h2>Productos seleccionados:</h2>
-        {SelectedProducts.map((product) => (
-          <div key={product.id}>
-            {product.name} ${product.price}
-          </div>
-        ))}
-      </div>
+        {/* Reemplaza la sección "sectionProductos" con el componente "SelectedProducts" */}
+        <SelectedProducts
+          filteredProducts={filteredProducts}
+          handleButtonClick={handleButtonClick}
+        />
+
+        <div className="selectedProducts">
+          <h2>Productos seleccionados:</h2>
+          {selectedProducts.map((product) => (
+            <div key={product.id}>
+              {product.name} ${product.price}
+            </div>
+          ))}
+        </div>
       </section>
     </div>
   );
